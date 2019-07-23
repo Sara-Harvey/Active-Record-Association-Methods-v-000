71189859c174230916ffbd22855f41483535a05c
Sara Harvey <saraharveynyc@gmail.com>
2019-07-23 14:50:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,7 +13,6 @@ class Song < ActiveRecord::Base
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
 
     self.artists.includes?
-    binding.pry
       if false 
       new_artist = Artist.create
       new_artist = self.artist
