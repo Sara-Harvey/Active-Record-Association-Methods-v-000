f0b318a9702440cc5d702391018b73145fb060c6
Sara Harvey <saraharveynyc@gmail.com>
2019-07-23 15:02:41 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,8 +12,7 @@ class Song < ActiveRecord::Base
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
 
-    self.artist.includes?(Artist)
-      if false  
+    self.artist.includes?(Artist) = false
         Artist.create = self.artist
         Artist.save
       else nil
